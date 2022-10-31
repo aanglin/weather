e7347e6ec2d188873193dac8611d8f1f86b355b6
Aaron Anglin <aaron.anglin101@gmail.com>
2022-10-31 12:15:14 -0500

    bad request on build

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,10 @@ export default function Home() {
   const [weather, setWeather] = useState({});
   const [getBackgroundImage, setGetBackgroundImage] = useState(Sunny)
   const [loading, setLoading] = useState(false);
+  const apiKey = process.env.NEXT_PUBLIC_KEY
   
   
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
  
 
   const getWeather = (e) => {
